f110efd1051ad1c8e39e505bff8dee75747579ec
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-10 16:14:06 +0200

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -25,7 +25,7 @@ favoriteNumber = _favoriteNumber;
 function addPerson(string memory _name, uint256 _favoriteNumber) public {
 people.push(People(_favoriteNumber, _name));
 nameToFavoriteNumber[_name] = _favoriteNumber;
-
+ 
 }
 }
 
