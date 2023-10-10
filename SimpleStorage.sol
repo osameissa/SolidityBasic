b731bd59805dc64d832509d8e308f6072daac3c3
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-10 15:23:21 +0200

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -4,7 +4,7 @@ pragma solidity ⌃0.8.8; // stable version
 // boolean, uint, int, address, bytes
 contract SimpleStorage {
 // defaults to null (0)
-uint256 favoriteNumber;
+uint256 public favoriteNumber;
 
 function store(uint256 _favoriteNumber) public {
 
