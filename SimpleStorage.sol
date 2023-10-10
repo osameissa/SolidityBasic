2e1b397390ea5bbdc78758b789c41fade8e188b1
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-10 15:20:46 +0200

    Update SimpleStorage.sol

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -1,1 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ⌃0.8.8; // stable version
+
+contract SimpleStorage {
+// boolean, uint, int, address, bytes
+// defaults to null (0)
+uint256 favoriteNumber;
+
+function store(uint256 _favoriteNumber) public {
+
+favoriteNumber = _favoriteNumber;
+
+}
 
