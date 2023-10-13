783babd0c673f8a8771dd4969699145f7fcc5b9f
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-13 23:15:43 +0200

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.8.8;
 import "@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
 // Why is this a library and not abstract?
-// Why not an interface?
+// Why not an interface? 
 library PriceConverter {
     // We could make this public, but then we'd have to deploy it
     function getPrice() internal view returns (uint256) {
