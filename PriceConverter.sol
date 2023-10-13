d6b222c4913d3b9b7b75bac4933a80aa5952e797
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-13 21:48:45 +0200

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -21,3 +21,12 @@ library PriceConverter {
     }
 
     // 1000000000
+    function getConversionRate(uint256 ethAmount)
+        internal
+        view
+        returns (uint256)
+    {
+        uint256 ethPrice = getPrice();
+        uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
+        // or (Both will do the same thing)
+  
