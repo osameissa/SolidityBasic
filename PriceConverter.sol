2795ecc2f438c9cf543741d1edac0ef42120c667
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-13 21:48:54 +0200

    Update PriceConverter.sol

--- PriceConverter.sol
+++ PriceConverter.sol
@@ -29,4 +29,8 @@ library PriceConverter {
         uint256 ethPrice = getPrice();
         uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1000000000000000000;
         // or (Both will do the same thing)
-  
+        // uint256 ethAmountInUsd = (ethPrice * ethAmount) / 1e18; // 1 * 10 ** 18 == 1000000000000000000
+        // the actual ETH/USD conversion rate, after adjusting the extra 0s.
+        return ethAmountInUsd;
+    }
+}
