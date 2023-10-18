182e048e9e5e8ec46653482f2077eda8a90e43d7
osameissa <113057277+osameissa@users.noreply.github.com>
2023-10-18 19:42:55 +0200

    Update deploy.js

--- deploy.js
+++ deploy.js
@@ -1,7 +1,7 @@
 const ethers = require("ethers")
 // const solc = require("solc")
 const fs = require("fs-extra")
-// in the futur
+// in the future u
 require("dotenv").config()
 
 async function main() {
